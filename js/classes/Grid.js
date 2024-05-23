4e538104d79cf46c1be5e0c87e9c90d75c74e50a
David-Asafe <davidasafe07@gmail.com>
2024-05-23 13:38:16 -0400

    add grid class

---
+++ js/classes/Grid.js
@@ -0,0 +1,30 @@
+class Grid {
+    constructor() {
+        this.position = {
+            x : 0,
+            y : 0
+        };
+        this.velocity = {
+            x : 3,
+            y : 0
+        };
+
+        this.invander = [];
+
+        const columns = Math.floor(Math.random() * 10 + 5);
+        const rows = Math.floor(Math.random() * 10 + 2);
+        this.width = columns * 30;
+
+        for (let i = 0; i < columns; i++) {
+            for (let j = 0; j < rows; j++) {
+                this.invander.push(new Invander({
+                    position: {
+                        i : i * 30,
+                        j : j * 30
+                    }
+                }));
+            }
+        }
+    
+    }
+}
