72d1d456986a4eb46ff2d3012a24fb90a4e9ef98
David-Asafe <davidasafe07@gmail.com>
2024-06-04 17:01:02 -0400

    add createParticles function in util.js file

--- js/util.js
+++ js/util.js
@@ -30,4 +30,24 @@ function rectnangularCollission({ rectangle1, rectangle2 }) {
     rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
     rectangle1.position.x <= rectangle2.position.x + rectangle2.width
   );
-} 
+}
+
+function createParticles({ objects, color, fades }) {
+  for (let i = 0; i < 15; i++) {
+    particles.push(
+      new Particle({
+        position: {
+          x: objects.position.x + objects.width / 2,
+          y: objects.position.y + objects.height / 2
+        },
+        velocity: {
+          x: (Math.round() - 0.5) * 2,
+          y: (Math.round() - 0.5) * 2
+        },
+        radius: Math.random() * 2,
+        color: color || "#BAA0DE",
+        fades: fades
+      })
+    );
+  }
+}
