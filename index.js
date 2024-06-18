4b87df66a970910e7bc409af6d0a94bc58870991
David-Asafe <davidasafe07@gmail.com>
2024-06-18 16:53:10 -0400

    add new enemy grids, fire projectiles from the player when the space key is pressed.

--- index.js
+++ index.js
@@ -357,21 +357,53 @@ for (let i = projectiles.length - 1; i >= 0; i--) {
         }) &&
         !game.over
       )
-      endGame();
+        endGame();
     }
   });
 
-  if(keys.ArrowLeft.pressed && player.position.x >= 0) {
+  if (keys.ArrowLeft.pressed && player.position.x >= 0) {
     player.velocity.x = -7;
     player.rotation = -0.15;
-  } else if(
+  } else if (
     keys.ArrowRight.pressed &&
     player.position.x + player.width <= canvas.width
   ) {
     player.velocity.x = 7;
     player.rotation = 0.15;
   } else {
-  player.velocity.x = 0;
-  player.rotation = 0;
+    player.velocity.x = 0;
+    player.rotation = 0;
   }
+
+  if (frames % randomInterval === 0) {
+    spawnBuffer = spawnBuffer < 0 ? 100 : spawnBuffer;
+    grids.push(new Grid());
+    randomInterval = Math.floor(Math.random() * 500 + spawnBuffer);
+    frames = 0;
+    spawnBuffer -= 100;
+  }
+
+  if (
+    keys.Space.pressed &&
+    player.powerUp === "Metralhadora" &&
+    frames % 2 === 0 &&
+    !game.over
+  ) {
+    if (frames % 6 === 0) audio.shoot.play();
+    projectiles.push(
+      new Projectile({
+        position: {
+          x: player.position.x + player.width / 2,
+          y: player.position.y
+        },
+        velocity: {
+          x: 0,
+          y: -10
+        },
+        color: "yellow"
+      })
+    );
+  }
+
+  frames++;
 }
